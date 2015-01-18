0873d5c1d7fd85f869388a31dcc9d99efa5ead2e
Lucas Vieira <lucasvg@poli.ufrj.br>
2015-01-18 18:42:07 -0200

    first commit

---
+++ sql.sql
@@ -0,0 +1,15 @@
+mysql -u root -p
+CREATE DATABASE db_challengeWebCERN;
+CREATE USER 'challengeWebCERN'@'localhost' IDENTIFIED BY 'pwd';
+GRANT ALL ON db_challengeWebCERN.* TO 'challengeWebCERN'@'localhost';
+FLUSH PRIVILEGES;
+mysql -u challengeWebCERN -p
+use db_challengeWebCERN
+
+CREATE TABLE IF NOT EXISTS `preference` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user` bigint(20) NOT NULL,
+  `setting` text NOT NULL,
+  `active` boolean NOT NULL,
+  PRIMARY KEY (`id`)
+)
